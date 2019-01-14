725bc9fda325a5c93e674f780ee50d90e563f8d9
seanmcquaid <sean.mcquaid2@verizon.net>
2019-01-14 11:13:06 -0500

    experimented with docs from node to show diff between ndoe and vanilla JS

--- firstFile.js
+++ firstFile.js
@@ -1,2 +1,39 @@
-const x = 1;
-console.log(x);
+// Native elements
+
+// const x = 1;
+// console.log(x);
+
+// let atlTeams = [
+//     "braves",
+//     "falcons",
+//     "hawks",
+//     "united"
+// ];
+
+// atmTeams = {
+//     fball: "falcons",
+//     bball: "hawks",
+//     baseball: "braves",
+//     futbol: "united",
+// }
+
+// console.log(window);
+// window.alert("I am in the browser!")
+// import DateTime
+// Like python, you can fetch modules
+// pip install pygame 
+// import pygame
+
+const os = require("os");
+
+// console.log(os);
+
+const cpus = os.cpus();
+// console.log(cpus);
+
+const freeMem = os.freemem();
+
+// console.log(freeMem);
+
+const nI = os.networkInterfaces();
+console.log(nI);
