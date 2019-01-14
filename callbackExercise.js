8674869fac8d6a3281151ef60c108bf748fb5a5b
seanmcquaid <sean.mcquaid2@verizon.net>
2019-01-14 15:52:17 -0500

    did last callback exercise

--- callbackExercise.js
+++ callbackExercise.js
@@ -15,6 +15,21 @@ function parameterFunction(callback){
 
 const array = [1,2,3,4];
 
-parameterFunction(array);
-parameterFunction(printFunction);
-parameterFunction("hello");
+// parameterFunction(array);
+// parameterFunction(printFunction);
+// parameterFunction("hello");
+
+
+function findEvens(array){
+    for(let i = 0; i < array.length; i++){
+        if(array[i] % 2 == 0){
+            return array[i];
+        } 
+    }
+}
+
+function findElement(array, callback){
+    return callback(array);
+}
+
+console.log(findElement([1,3,5,8,9,10], findEvens));
